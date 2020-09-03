23a3ccb888f0912f7cc78dfb7961a1838ba9109c
ArindraRevaM <reremaulana717@gmail.com>
2020-09-03 17:27:43 +0700

    Menambah fungsi READ

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -8,6 +8,42 @@ use Illuminate\Support\Facades\Validator;
 
 class OrdersController extends Controller
 {
+	 public function show()
+    {
+        $data_orders = Orders::join('customers', 'customers.id_customers', 'orders.id_customers')->get();
+        return Response()->json($data_orders);
+    }
+     public function show()
+    {
+        $data_orders = Siswa::join('product', 'product.id_product', 'orders.id_product')->get();
+        return Response()->json($data_orders);
+    }
+    public function detail($id)
+    {
+        if(Orders::where('id', $id)->exists()){
+            $data_orders = Orders::join('customers', 'customers.id_customers', 'orders.id_kelas')
+                                      ->where('orders.id', '=', $id)
+                                      ->get();
+
+            return Response()->json($data_orders);  
+        }
+        else{
+            return Response()->json(['message' => 'Tidak Ditemukan']);
+        }
+    }
+    public function detail($id)
+    {
+        if(Orders::where('id', $id)->exists()){
+            $data_orders = Orders::join('product', 'product.id_product', 'orders.id_product')
+                                      ->where('orders.id', '=', $id)
+                                      ->get();
+
+            return Response()->json($data_orders);  
+        }
+        else{
+            return Response()->json(['message' => 'Tidak Ditemukan']);
+        }
+    }
    public function store(Request $request)
     {
     	$validator=Validator::make($request->all(),
