380af2341cdc56f2ba9f87f7da252b920bdf9e5b
ArindraRevaM <reremaulana717@gmail.com>
2020-10-22 08:02:58 +0700

    Menambah pembagian hak akses

--- routes/api.php
+++ routes/api.php
@@ -17,17 +17,31 @@ Route::post('/login', 'UserController@login');
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 
+Route::group(['middleware' => ['api.superadmin']], function ()
+{
+	Route::delete('/customers/{id}', 'CustomersController@destroy');
+	Route::delete('/product/{id}', 'ProductController@store');
+	Route::delete('/orders/{id}', 'OrdersController@delete');
+});
+
+Route::group(['middleware' => ['api.admin']], function ()
+{
+	Route::post('/customers', 'CustomersController@store');
+	Route::put('/customers/{id}', 'CustomersController@update');
+
+	Route::post('/product', 'ProductController@store');
+	Route::put('/product/{id}', 'ProductController@update');
+
+	Route::post('/orders', 'OrdersController@store');
+	Route::put('/orders/{id}', 'OrdersController@update');
+});
+
 Route::get('/customers', 'CustomersController@show');
-Route::post('/customers', 'CustomersController@store');
-Route::delete('/customers/{id}', 'CustomersController@destroy');
+Route::get('/customers/{id}', 'CustomersController@detail');
 
 Route::get('/product', 'ProductController@show');
-Route::post('/product', 'ProductController@store');
-Route::delete('/product/{id}', 'ProductController@store');
+Route::get('/product/{id}', 'ProductController@detail');
 
 Route::get('/orders', 'OrdersController@show');
 Route::get('/orders/{id}', 'OrdersController@detail');
-Route::post('/orders', 'OrdersController@store');
-Route::put('/orders/{id}', 'OrdersController@update');
-Route::delete('/orders/{id}', 'OrdersController@delete');
 });
